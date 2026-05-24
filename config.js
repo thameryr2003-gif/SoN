276245c761d96d1a0086f192696be0b2299faf82
thameryr2003-gif <thameryr2003@gmail.com>
2026-05-24 03:11:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ Copyright (c) 2024 Wick Studio
 
 module.exports = {    
     token: "", // token
-    clientId: "", // bot id
+    clientId: "1461860525406294159", // bot id
     prefix: "!", // prefix
     language: "ar", // ar for arabic | en for english
     verbose: true,
@@ -18,7 +18,7 @@ module.exports = {
     enableLogging: true,
     djRoleName: "Wick",
     aliases: {
-      play: ["p", "start", "playmusic"],
+      play: ["ت", "start", "playmusic"],
       pause: ["hold", "stopmusic"],
       resume: ["r", "continue"],
       skip: ["s", "next", "jump"],
